12ebfbf334b596a02c995432337caa472ce73725
Juan Perata <juan@bespoken.tools>
2018-01-25 11:39:51 -0500

    Add Index.ts, remove unnecessary test files

---
+++ src/Index.ts
@@ -0,0 +1,7 @@
+export {BuiltinSlotTypes} from "./BuiltinSlotTypes";
+export {BuiltinUtterances} from "./BuiltinUtterances";
+export {Intent, IIntentSchema} from "./IIntentSchema";
+export {IModel} from "./IModel";
+export {SampleUtterances} from "./SampleUtterances";
+export {SlotTypes} from "./SlotTypes";
+export {Utterance} from "./Utterance";
