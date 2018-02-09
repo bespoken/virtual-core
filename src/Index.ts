a39bf52b0a0ba4577b3165f02c27bb43ab53c846
Juan Perata <juan@bespoken.tools>
2018-02-09 09:25:26 -0500

    Export Slot Interfaces

--- src/Index.ts
+++ src/Index.ts
@@ -3,5 +3,5 @@ export {BuiltinUtterances} from "./BuiltinUtterances";
 export {IIntentSchema, Intent, IntentSlot} from "./IIntentSchema";
 export {IModel} from "./IModel";
 export {SampleUtterances} from "./SampleUtterances";
-export {SlotTypes} from "./SlotTypes";
+export {ISlotType, ISlotValue, ISlotValueName, SlotMatch, SlotType, SlotTypes} from "./SlotTypes";
 export {Utterance} from "./Utterance";
